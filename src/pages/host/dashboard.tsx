66a812634fa4f5d10c6f3753b914e67a5a7efd70
Tauhid <tauhidahmed716@gmail.com>
2024-10-23 23:39:58 +0600

    add sub route link component to host route

--- src/pages/host/dashboard.tsx
+++ src/pages/host/dashboard.tsx
@@ -1,3 +1,31 @@
+import { Link, Outlet } from "react-router-dom";
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div className="py-10 container">
+      <ul className="flex gap-4">
+        <li>
+          <Link className="active:underline hover: underline" to="/host">
+            Dashboard
+          </Link>
+        </li>
+        <li>
+          <Link className="active:underline hover: underline" to="/host/income">
+            Income
+          </Link>
+        </li>
+        <li>
+          <Link className="active:underline hover: underline" to="/host/vans">
+            Vans
+          </Link>
+        </li>
+        <li>
+          <Link className="active:underline hover: underline" to="/host/reviews">
+            Reviews
+          </Link>
+        </li>
+      </ul>
+      <Outlet />
+    </div>
+  );
 }
