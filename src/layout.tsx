a4d9caf25cba29248bc8678c39db52367c603f6d
Tauhid <tauhidahmed716@gmail.com>
2024-10-23 16:15:34 +0600

    add container utility class and main site bg-color also refactor main layout component

--- src/layout.tsx
+++ src/layout.tsx
@@ -4,22 +4,26 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="min-h-screen flex flex-col px-4 max-w-6xl mx-auto bg-background">
-      {children}
+    <div className="bg-background">
+      <div className="min-h-screen flex flex-col">{children}</div>
     </div>
   );
 }
 
 function Header({ children }: { children: React.ReactNode }) {
-  return <div className="py-4">{children}</div>;
+  return <div className="py-4 h-16">{children}</div>;
 }
 
 function Body({ children }: { children: React.ReactNode }) {
-  return <main className="">{children}</main>;
+  return <main className="flex-1">{children}</main>;
 }
 
 function Footer({ children }: { children: React.ReactNode }) {
-  return <div className="text-center py-4 mt-auto">{children}</div>;
+  return (
+    <div className="text-center py-4 mt-auto h-16 flex items-center justify-center">
+      {children}
+    </div>
+  );
 }
 
 Layout.displayName = "Root Layout";
