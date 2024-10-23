adbbfca170944e1e28cfcc400cc100047b21cbec
Tauhid <tauhidahmed716@gmail.com>
2024-10-24 00:25:43 +0600

    add dashboard page as host subroute
    - was implemented for main host layout file
    - now removed

--- src/pages/host/dashboard.tsx
+++ src/pages/host/dashboard.tsx
@@ -1,31 +1,3 @@
-import { Link, Outlet } from "react-router-dom";
-
 export default function Dashboard() {
-  return (
-    <div className="py-10 container">
-      <ul className="flex gap-4">
-        <li>
-          <Link className="active:underline hover: underline" to="/host">
-            Dashboard
-          </Link>
-        </li>
-        <li>
-          <Link className="active:underline hover: underline" to="/host/income">
-            Income
-          </Link>
-        </li>
-        <li>
-          <Link className="active:underline hover: underline" to="/host/vans">
-            Vans
-          </Link>
-        </li>
-        <li>
-          <Link className="active:underline hover: underline" to="/host/reviews">
-            Reviews
-          </Link>
-        </li>
-      </ul>
-      <Outlet />
-    </div>
-  );
+  return <div>Dashboard</div>;
 }
