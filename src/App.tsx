337d4df908871a291168f7fa977e2e7cf166873d
Tauhid <tauhidahmed716@gmail.com>
2024-10-23 16:13:58 +0600

    link homepage to main router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./layout";
+import Header from "./components/header";
+import Homepage from "./pages/home";
 
 export default function App() {
   return (
     <Layout>
       <Router>
-        <Layout.Header>Header</Layout.Header>
+        <Layout.Header>
+          <Header />
+        </Layout.Header>
         <Layout.Body>
           <Routes>
-            <Route path="/" element={<>dsd</>} />
+            <Route path="/" element={<Homepage />} />
           </Routes>
         </Layout.Body>
         <Layout.Footer>Footer</Layout.Footer>
