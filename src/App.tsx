cad6d1e4ed40b6c0fa1d8ba61f27a341b835a943
Tauhid <tauhidahmed716@gmail.com>
2024-10-23 23:03:21 +0600

    loyout organize
    - layout organized with layout component and react-router-dom outlet
    - prev code layout component was messy not for using outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./layout";
-import Header from "./components/header";
 import Homepage from "./pages/home";
 import Vans from "./pages/vans";
-import Footer from "./components/footer";
 import AboutPage from "./pages/about";
 import VanDetails from "./pages/van-details";
 
 export default function App() {
   return (
-    <Layout>
-      <Router>
-        <Layout.Header>
-          <Header />
-        </Layout.Header>
-        <Layout.Body>
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/vans" element={<Vans />} />
-            <Route path="/vans/:id" element={<VanDetails />} />
-            <Route path="/about" element={<AboutPage />} />
-          </Routes>
-        </Layout.Body>
-        <Layout.Footer>
-          <Footer />
-        </Layout.Footer>
-      </Router>
-    </Layout>
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetails />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
--- src/layout.tsx
+++ src/layout.tsx
@@ -1,32 +1,19 @@
-interface LayoutProps {
-  children: React.ReactNode;
-}
+import Header from "./components/header";
+import Footer from "./components/footer";
+import { Outlet } from "react-router-dom";
 
-export default function Layout({ children }: LayoutProps) {
+export default function Layout() {
   return (
-    <div className="bg-background">
-      <div className="min-h-screen flex flex-col">{children}</div>
+    <div className="bg-background min-h-screen flex flex-col">
+      <div className="py-4 h-16">
+        <Header />
+      </div>
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <footer className="text-center py-4 mt-auto h-16 flex items-center justify-center text-orange-100 bg-neutral-900">
+        <Footer />
+      </footer>
     </div>
   );
 }
-
-function Header({ children }: { children: React.ReactNode }) {
-  return <div className="py-4 h-16">{children}</div>;
-}
-
-function Body({ children }: { children: React.ReactNode }) {
-  return <main className="flex-1">{children}</main>;
-}
-
-function Footer({ children }: { children: React.ReactNode }) {
-  return (
-    <footer className="text-center py-4 mt-auto h-16 flex items-center justify-center text-orange-100 bg-neutral-900">
-      {children}
-    </footer>
-  );
-}
-
-Layout.displayName = "Root Layout";
-Layout.Header = Header;
-Layout.Body = Body;
-Layout.Footer = Footer;
