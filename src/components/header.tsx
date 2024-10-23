4326175f40b0233e3c8124a56edea405061cc971
Tauhid <tauhidahmed716@gmail.com>
2024-10-23 23:10:08 +0600

    add hostpage reference

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,10 +3,11 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header className="flex justify-between items-center gap-4 container">
-      <Link className="font-bold text-2xl mr-auto" to="/">
+      <Link className="font-bold text-2xl mr-auto hover:underline" to="/">
         #VANLIFE
       </Link>
       <div className="contents">
+        <Link to="/host">Host</Link>
         <Link to="/about">About</Link>
         <Link to="/vans">Vans</Link>
       </div>
