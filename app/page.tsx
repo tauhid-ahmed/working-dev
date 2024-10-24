0a448d6c58bccd94db2eb9c11c29bfdb26bb487a
Tauhid <tauhidahmed716@gmail.com>
2024-10-24 12:46:07 +0600

    customize button component

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      Home <Button>Click</Button>
+      Home
+      <Button>Click</Button>
+      <Button variant={"destructive"}>Click</Button>
     </div>
   );
 }
