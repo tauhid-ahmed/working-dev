0c1ad3994f1a9dbe550f3bcef130fcd12dd72c1f
Tauhid <tauhidahmed716@gmail.com>
2024-10-24 20:29:36 +0600

    signin and signup form validation implements

--- features/auth/components/sign-in-card.tsx
+++ features/auth/components/sign-in-card.tsx
@@ -1,30 +1,78 @@
+"use client";
 import { FcGoogle as GoogleIcon } from "react-icons/fc";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 
+import { useForm } from "react-hook-form";
+import { signInSchema } from "../schema/sign-in-schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
 export default function SignInCard() {
+  const form = useForm({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   return (
     <Card className="size-full border-none shadow-none max-w-lg py-10">
       <CardHeader>
         <CardTitle className="text-2xl text-center">Welcome Back</CardTitle>
       </CardHeader>
       <CardContent>
-        <form className="space-y-6">
-          <Input placeholder="Enter email address" />
-          <Input placeholder="Enter password" />
-          <div className="space-y-2">
-            <Button className="w-full" size={"lg"}>
-              Login
-            </Button>
-            <span className="block text-center">or</span>
-            <Button variant="secondary" size="lg" className="w-full">
-              <GoogleIcon />
-              Login with Google
-            </Button>
-          </div>
-        </form>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit((values) => console.log(values))}
+            className="space-y-6">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="Enter email address" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="Enter password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="space-y-2">
+              <Button className="w-full" size={"lg"}>
+                Login
+              </Button>
+              <span className="block text-center">or</span>
+              <Button variant="secondary" size="lg" className="w-full">
+                <GoogleIcon />
+                Login with Google
+              </Button>
+            </div>
+          </form>
+        </Form>
       </CardContent>
     </Card>
   );
--- features/auth/components/sign-up-card.tsx
+++ features/auth/components/sign-up-card.tsx
@@ -1,40 +1,100 @@
-import { FcGoogle as GoogleIcon } from "react-icons/fc";
+"use client";
 
+import { FcGoogle as GoogleIcon } from "react-icons/fc";
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Checkbox } from "@/components/ui/checkbox";
 import Link from "next/link";
+import { signUpSchema } from "../schema/sign-up-schema";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 export default function SignUpCard() {
+  const form = useForm({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
   return (
     <Card className="size-full border-none shadow-none max-w-lg py-10">
       <CardHeader>
         <CardTitle className="text-2xl text-center">Sign Up</CardTitle>
       </CardHeader>
       <CardContent>
-        <form className="space-y-6">
-          <Input placeholder="Enter email address" />
-          <Input placeholder="Enter password" />
-          <Input placeholder="Enter confirm password" />
-          <p className="flex items-center gap-1">
-            <Checkbox className="border-blue-500 size-4 data-[state=checked]:bg-blue-600" />{" "}
-            I agree to the terms of service and{" "}
-            <Link className="font-medium text-blue-600" href="">
-              privacy policy
-            </Link>
-          </p>
-          <div className="space-y-2">
-            <Button className="w-full" size={"lg"}>
-              Sign up
-            </Button>
-            <span className="block text-center">or</span>
-            <Button variant="secondary" size="lg" className="w-full">
-              <GoogleIcon />
-              Signup with Google
-            </Button>
-          </div>
-        </form>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit((values) => console.log(values))}
+            className="space-y-6">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="Enter email address" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="Enter password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="Enter confirm password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <p className="flex items-center gap-2">
+              <Checkbox className="border-blue-500 size-4 data-[state=checked]:bg-blue-600" />{" "}
+              <span className="text-sm">
+                I agree to the terms of service and{" "}
+                <Link className="font-medium text-blue-600" href="">
+                  privacy policy
+                </Link>
+              </span>
+            </p>
+            <div className="space-y-2">
+              <Button className="w-full" size={"lg"}>
+                Sign up
+              </Button>
+              <span className="block text-center">or</span>
+              <Button variant="secondary" size="lg" className="w-full">
+                <GoogleIcon />
+                Signup with Google
+              </Button>
+            </div>
+          </form>
+        </Form>
       </CardContent>
     </Card>
   );
